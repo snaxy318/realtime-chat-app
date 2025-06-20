fe08cccbeb53c8fa9ef9406c7bcc4cb4d9b60394
snaxy318 <suryanshtejas@gmail.com>
2025-06-20 22:53:20 +0530

    Setting up backend

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log("A user connected");
+
+    socket.on("message", (msg) => {
+        io.emit("message", msg);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+    });
+});
+
+app.get("/", (req, res) => {
+    res.send("Chat server running!");
+});
+
+server.listen(5000, () => {
+    console.log("Server listening on port 5000");
+});
