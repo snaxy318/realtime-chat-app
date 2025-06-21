08d206bbd6d746d8bf3f80e3c5afa5c083c86ad1
snaxy318 <suryanshtejas@gmail.com>
2025-06-21 18:54:49 +0530

    converted to ES modules

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,32 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
+
 const io = new Server(server, {
-    cors: {
-        origin: "*"
-    }
+  cors: {
+    origin: "*",
+  },
 });
 
 io.on("connection", (socket) => {
-    console.log("A user connected");
+  console.log("A user connected");
 
-    socket.on("message", (msg) => {
-        io.emit("message", msg);
-    });
+  socket.on("message", (msg) => {
+    io.emit("message", msg);
+  });
 
-    socket.on("disconnect", () => {
-        console.log("User disconnected");
-    });
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
 });
 
 app.get("/", (req, res) => {
-    res.send("Chat server running!");
+  res.send("Chat server running!");
 });
 
 server.listen(5000, () => {
-    console.log("Server listening on port 5000");
+  console.log("Server listening on port 5000");
 });
